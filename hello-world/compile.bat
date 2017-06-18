21ab1786e3d7d9da2568d347e5a20157c9db1a89
Mattias Buelens <mattias.buelens@gmail.com>
2017-06-18 18:49:03 +0200

    Use proper file extensions.

--- hello-world/compile.bat
+++ hello-world/compile.bat
@@ -7,5 +7,5 @@ if not exist "%outdir%" mkdir "%outdir%"
 
 rustc --target wasm32-unknown-emscripten --crate-type lib --emit llvm-bc "src/lib.rs" -o "%outdir%/lib.bc"
 "%wasmtools%/llc" "%outdir%/lib.bc" -march=wasm32 -filetype=asm -asm-verbose=false -thread-model=single -data-sections -function-sections -o "%outdir%/lib.s"
-"%wasmtools%/s2wasm" "%outdir%/lib.s" --import-memory --allocate-stack 10000 -o "%outdir%/lib.wasm"
-"%wasmtools%/wasm-opt" "%outdir%/lib.wasm" -o "%outdir%/lib-opt.wasm"
+"%wasmtools%/s2wasm" "%outdir%/lib.s" --import-memory --allocate-stack 10000 -o "%outdir%/lib.wast"
+"%wasmtools%/wasm-opt" "%outdir%/lib.wast" -o "%outdir%/lib.wasm"
