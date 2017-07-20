4116b53b482d74b24647c9873747b67ef386a1c1
Mattias Buelens <mattias@buelens.com>
2017-07-20 21:45:31 +0200

    Use no_std and no_main.

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,9 +1,17 @@
+#![feature(lang_items, core_intrinsics)]
+#![feature(start)]
+#![no_std]
+#![no_main]
+use core::intrinsics;
+
 mod webassembly;
 
 use webassembly::console;
 use webassembly::math;
 
-pub fn main() {
+// Entry point for this program.
+#[no_mangle] // ensure that this symbol is called `main` in the output
+pub extern fn main(_argc: i32, _argv: *const *const u8) -> i32 {
     console::log("Hello, WebAssembly!");;
 
     let x = math::sqrt(17.0);
@@ -12,4 +20,28 @@ pub fn main() {
     } else {
         console::warn("Alert!");
     }
+
+    0
+}
+
+// These functions are used by the compiler, but not
+// for a bare-bones hello world. These are normally
+// provided by libstd.
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern fn rust_eh_personality() {
+}
+
+// This function may be needed based on the compilation target.
+#[lang = "eh_unwind_resume"]
+#[no_mangle]
+pub extern fn rust_eh_unwind_resume() {
+}
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn rust_begin_panic(_msg: core::fmt::Arguments,
+                               _file: &'static str,
+                               _line: u32) -> ! {
+    unsafe { intrinsics::abort() }
 }
